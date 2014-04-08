d69ec64f68777399ec5cb7b3149007813c226fed
GTAPeople <gtapeople@hotmail.com>
2014-04-08 12:09:20 -0500

    Añadir sierra y su script.

---
+++ Assets/Scripts/Sawblade.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sawblade : MonoBehaviour {
+
+	public float speed = 300f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.forward*speed*Time.deltaTime,Space.World);
+	}
+
+	void OnTriggerEnter2D(Collider2D c){
+		if(c.tag =="Player"){
+			Debug.Log("DIE!");
+		}
+	}
+}
