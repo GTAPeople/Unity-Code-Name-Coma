77b244fb4c71bfc9af610efdfb108ebd2f24d7f8
GTAPeople <gtapeople@hotmail.com>
2014-04-12 15:29:59 -0500

    Mover la cámara solo a la derecha, no izquierda.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
-	public float xMargin = 1f;		// Distance in the x axis the player can move before the camera follows.
-	public float yMargin = 1f;		// Distance in the y axis the player can move before the camera follows.
-	public float xSmooth = 8f;		// How smoothly the camera catches up with it's target movement in the x axis.
-	public float ySmooth = 8f;		// How smoothly the camera catches up with it's target movement in the y axis.
+	public float xMargin = 2f;		// Distance in the x axis the player can move before the camera follows.
+	public float yMargin = 2f;		// Distance in the y axis the player can move before the camera follows.
+	public float xSmooth = 4f;		// How smoothly the camera catches up with it's target movement in the x axis.
+	public float ySmooth = 4f;		// How smoothly the camera catches up with it's target movement in the y axis.
 	public GameObject deadStart;	// The GameObject from the start level scene, the camera can have.
 	public GameObject deadEnd;		// The GameObject from the end level scene, the camera can have.
 	public float maxY;				// The maximum y coordinate the camera can have.
@@ -14,15 +14,15 @@ public class CameraFollow : MonoBehaviour {
 
 	private float maxX;				// The maximum x coordinate the camera can have.
 	private float minX;				// The minimum x coordinate the camera can have.
-	
+	private float horzExtent;
 	
 	private Transform player;		// Reference to the player's transform.
 
 	// Use this for initialization
 	void Start () {
 		float vertExtent = Camera.main.camera.orthographicSize;  
-		float horzExtent = vertExtent * Camera.main.pixelWidth / Camera.main.pixelHeight;
-		minX = horzExtent + deadStart.transform.position.x + (deadEnd.GetComponent<BoxCollider2D>().size.x/2);
+		horzExtent = vertExtent * Camera.main.pixelWidth / Camera.main.pixelHeight;
+		minX = horzExtent + deadStart.transform.position.x + (deadStart.GetComponent<BoxCollider2D>().size.x/2);
 		maxX = deadEnd.transform.position.x - deadEnd.GetComponent<BoxCollider2D>().size.x/2 - horzExtent;
 //		Debug.Log (Screen.width);
 	}	
@@ -57,17 +57,29 @@ public class CameraFollow : MonoBehaviour {
 		// By default the target x and y coordinates of the camera are it's current x and y coordinates.
 		float targetX = transform.position.x;
 		float targetY = transform.position.y;
+
+		// By default for deadStart
+		float targetDeadStartX = deadStart.transform.position.x;
 		
 		// If the player has moved beyond the x margin...
-		if(CheckXMargin())
-			// ... the target x coordinate should be a Lerp between the camera's current x position and the player's current x position.
-			targetX = Mathf.Lerp(transform.position.x, player.position.x, xSmooth * Time.deltaTime);
-		
+		if(CheckXMargin()){
+			// To make the camera move right, not left.
+			if(player.position.x>transform.position.x){
+				// ... the target x coordinate should be a Lerp between the camera's current x position and the player's current x position.
+				targetX = Mathf.Lerp(transform.position.x, player.position.x, xSmooth * Time.deltaTime);
+				targetDeadStartX = transform.position.x-horzExtent - (deadStart.GetComponent<BoxCollider2D>().size.x/2);
+			}
+		}
 		// If the player has moved beyond the y margin...
-		if(CheckYMargin())
+		if(CheckYMargin()){
 			// ... the target y coordinate should be a Lerp between the camera's current y position and the player's current y position.
 			targetY = Mathf.Lerp(transform.position.y, player.position.y, ySmooth * Time.deltaTime);
-		
+		}
+
+		// Move deadStart while player move right
+//		Debug.Log (targetDeadStartX);
+		deadStart.transform.position = new Vector3(targetDeadStartX,deadStart.transform.position.y,deadStart.transform.position.z);
+
 		// The target x and y coordinates should not be larger than the maximum or smaller than the minimum.
 		targetX = Mathf.Clamp(targetX, minX, maxX);
 		targetY = Mathf.Clamp(targetY, minY, maxY);
